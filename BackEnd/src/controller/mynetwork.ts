04a2fede48233df887da391cd4307e230c31432c
juyeon <honorskim@nate.com>
2022-01-19 16:28:49 +0900

    오류 수정

--- BackEnd/src/controller/mynetwork.ts
+++ BackEnd/src/controller/mynetwork.ts
@@ -2,17 +2,15 @@ import express from "express";
 import * as dataCtrl from "data/mynetwork";
 
 export function findMyNetworkUserAdd(req: express.Request, res: express.Response) {
-    dataCtrl.findMyNetwork(req.body)
+    dataCtrl.findMyNetwork(req.body.email)
         .then(() => {
-            for (const value of req.body.meetpeople) {
-                dataCtrl.saveMyNetworkUserIncrease(req.body.email, value)
-                .then(() => { return res.status(200).json({ message: "mynetwork find and increase meetcount" })} )
+                dataCtrl.saveMyNetworkUserIncrease(req.body.email, req.body.meetpeople[0])
+                .then((retData) => { return res.status(200).json({ message: "mynetwork find and increase meetcount" }) })
                 .catch(() => { 
-                    dataCtrl.saveMyNetworkUserAdd(req.body.email, value)
+                    dataCtrl.saveMyNetworkUserAdd(req.body.email, req.body.meetpeople[0])
                     .then(() => { return res.status(200).json({ message: "mynetwork push" })} )
                     .catch(() => { return res.status(500).json({ message: "mynetwork push error" })} );
                 });
-            } // for
         })
         .catch(() => {
             dataCtrl.saveMyNetwork(req.body)
@@ -24,6 +22,7 @@ export function findMyNetworkUserAdd(req: express.Request, res: express.Response
 export function findMyNetwork(req: express.Request, res: express.Response) {
     const email = req.body.email;
     dataCtrl.findMyNetwork(email)
-        .then((retData) => { return res.status(200).json({ message: "mynetwork find", data: retData }) })
+        .then((retData) => { 
+            return res.status(200).json({ message: "mynetwork find", data: retData }) })
         .catch((error) => { return res.status(404).json({ message: error.message }) });
 }
