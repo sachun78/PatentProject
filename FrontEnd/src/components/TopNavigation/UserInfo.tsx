6ca86153d934441483c9fb1d330426e281543fd8
n27 <skfdkfkalsrn@gmail.com>
2022-02-15 16:29:15 +0900

    userinfo 드랍다운으로 변경

--- FrontEnd/src/components/TopNavigation/UserInfo.tsx
+++ FrontEnd/src/components/TopNavigation/UserInfo.tsx
@@ -1,8 +1,14 @@
 import { css } from '@emotion/react'
-import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useRef} from 'react'
 import { useUserState } from '../../atoms/authState'
 import useAuth from '../../hooks/useAuth'
+import palette from '../../lib/palette'
+import useToggle from '../../hooks/useToggle'
+import {
+  MdArrowDropDown
+} from 'react-icons/md'
+import CategoryPicker from './UserInfoPicker'
+import { BiUser } from "react-icons/bi";
 
 export type UserInfoProps = {}
 
@@ -10,10 +16,28 @@ function UserInfo({}: UserInfoProps) {
   const [user] = useUserState()
   const { logout } = useAuth()
   const username: string = user?.username || ''
+  const [categoryPicker, toggleCategoryPicker] = useToggle(false)
+  const timeframeRef = useRef<HTMLDivElement | null>(null)
+  const selectItem =[[0, username], [1, 'logout']]
+
+  const onClose = (e: React.MouseEvent<HTMLElement>) => {
+    if (!timeframeRef.current) return
+    if (
+      e.target === timeframeRef.current ||
+      timeframeRef.current.contains(e.target as Node)
+    ) {
+      return
+    }
+    toggleCategoryPicker()
+  }
 
   return <div css={wrapper}>
-    <Link to={'/profile'}>{username}</Link>
-    <Link css={logoutStyle} to={'/login'} onClick={logout}>logout</Link>
+    <div css={HomeTabStyle} onClick={toggleCategoryPicker} ref={timeframeRef} >
+      <BiUser />      
+      <MdArrowDropDown />
+    </div>
+
+    <CategoryPicker onClose={onClose} visible={categoryPicker} />
   </div>
 }
 
@@ -25,13 +49,42 @@ const wrapper = css`
   align-items: center;
   justify-content: flex-end;
   margin-right: 2rem;
+  
 `
 
 const logoutStyle = css`
   margin-left: 1rem;
   font-size: 1.25rem;
   text-decoration: none;
-  font-weight: 600;
+  font-weight: 600;  
 `
+const HomeTabStyle = css`
+  background: ${palette.grey[50]};  
+  height: 3.2rem;
+  width: 9.6rem;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 0.8rem;
+  padding-right: 0.8rem;
+  font-weight: 600;
+  color: ${palette.blueGrey[600]};
+  font-size: 1.4rem;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.05);
+  
+
+  svg {
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+
+  cursor: pointer;
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      opacity: 0.75;
+    }
+  }
+  `
 
 export default UserInfo
---
+++ FrontEnd/src/components/TopNavigation/UserInfoPicker.tsx
@@ -0,0 +1,94 @@
+import { css } from '@emotion/react'
+import palette from '../../lib/palette'
+import React, { useState } from 'react'
+import { useUserState } from '../../atoms/authState'
+import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
+
+
+
+export type CategoryPickerProps = {
+  visible: boolean;
+  onClose: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+function CategoryPicker({ visible, onClose }: CategoryPickerProps) {
+  const [user] = useUserState()
+  const username: string = user?.username || ''
+  const selectItem = [[0, username], [1, 'logout']]
+  const { logout } = useAuth()
+  const [select, setSelect] = useState(0)
+  
+  return <>
+    {visible ? <div css={wrapper}>
+      <div css={blockStyle} onClick={onClose}>
+        <ul>
+          <li>
+            <Link css={profileStyle} to={'/profile'}>{username}</Link>            
+          </li>
+          <li>
+            <Link css={logoutStyle} to={'/login'} onClick={() => logout}>logout</Link>
+          </li>               
+        </ul>
+      </div>
+    </div> : null}
+  </>
+}
+
+const wrapper = css`
+  position: absolute;
+  right: 0;
+  top: 100%;
+  z-index: 5;`
+
+const blockStyle = css`
+  
+  margin-right: 1rem;
+  width: 10.2rem;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.1);
+  background: ${palette.grey[50]};
+  color: ${palette.blueGrey[600]};
+  transform-origin: top right;
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
+
+  li {
+    text-align: center;
+    cursor: pointer;
+
+    &:hover {
+      background: ${palette.grey[100]};
+    }
+
+    font-weight: 600;
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+
+    &.active {
+      color: ${palette.blueGrey[900]};
+    }
+  }
+
+  li + li {
+    border-top: 1px solid ${palette.grey[100]};
+  }
+`
+const profileStyle = css `  
+  margin-left: 1rem;
+  font-size: 1.25rem;
+  text-decoration: none;  
+  font-weight: 600;  
+
+`
+const logoutStyle = css`
+  margin-left: 1rem;
+  font-size: 1.25rem;
+  text-decoration: none;
+  font-weight: 600;  
+`
+
+export default CategoryPicker
