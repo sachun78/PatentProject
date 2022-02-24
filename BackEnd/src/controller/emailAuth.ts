f5260959d6e1cdb734415fb834cdfc9efff0e405
BaeHyukJun <sachun78@naver.com>
2022-02-24 16:59:26 +0900

    * 작업 내용 : auth email verify시에 logged 체크 부분으로 인한 오류 수정

--- BackEnd/src/controller/emailAuth.ts
+++ BackEnd/src/controller/emailAuth.ts
@@ -47,20 +47,30 @@ export async function sendAuthEmail(req: IRequest, res: Response) {
 export async function isVerifyMail(req: IRequest, res: Response) {
   const reqCode: string = req.params.code;
   
-  const authmail = await EmaiAuthlRepo.updateAuthMail(reqCode, {logged: true});
-  if (!authmail) {
-    return res.status(404).json({ message: 'Email info not found' });
-  }
-
-  const updateUser = await authRepo.updateUser(authmail.userid, {certified: true});
-  if (!updateUser) {
-    return res.status(401).json({ message: 'Fail user info update'});
-  }
+  try {
+    const mail = await EmaiAuthlRepo.findAuthMail(reqCode);
+    if (!mail) {
+      return res.status(404).json({ message: 'Email info not found' });
+    }
   
-  const diff = new Date().getTime() - new Date(authmail.createdAt).getTime();
-  if (diff > 1000 * 60 * 60 * 24 || authmail.logged) {
-    return res.status(410).json({ message: 'Expired code'});
-  }
+    const diff = new Date().getTime() - new Date(mail.createdAt).getTime();
+    if (diff > 1000 * 60 * 60 * 24 || mail.logged) {
+      return res.status(410).json({ message: 'Expired code'});
+    }
+  
+    const authmail = await EmaiAuthlRepo.updateAuthMail(reqCode, {logged: true});
+    if (!authmail) {
+      return res.status(404).json({ message: 'Email info not found' });
+    }
+  
+    const updateUser = await authRepo.updateUser(authmail.userid, {certified: true});
+    if (!updateUser) {
+      return res.status(401).json({ message: 'Fail user info update'});
+    }
 
-  res.status(200).json({email: authmail.email, certified: updateUser.certified});  
+    res.status(200).json({email: authmail.email, certified: updateUser.certified});
+  } catch(e) {
+    console.error(e);
+    res.status(401).send('forbidden');
+  }  
 }
