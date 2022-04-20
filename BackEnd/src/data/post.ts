20fdce17c8b0492396b5dc2badff81a3433880d5
BaeHyukJun <sachun78@naver.com>
2022-04-20 16:59:35 +0900

    * fixed  - one post item get

--- BackEnd/src/data/post.ts
+++ BackEnd/src/data/post.ts
@@ -3,6 +3,7 @@ import { useVirtualId } from 'database/database'
 
 type PostType = {
   owner_id: string;
+  owner_username: string;
   owner_thumb: string;
   contents: string;
   images: string[];
@@ -12,6 +13,7 @@ type PostType = {
 
 type CommentType = {
   owner_id: string;
+  owner_username: string;
   ower_thumb: string;
   contents: string;
   createdAt: Date;
@@ -19,6 +21,7 @@ type CommentType = {
 
 const postSchema = new mongoose.Schema<PostType>({
   owner_id: { type: String, required: true },
+  owner_username: { type: String, required: true },
   owner_thumb: { type: String, default: '' },
   contents: { type: String, required: true },
   images: { type: [String], default: [] },
@@ -26,6 +29,7 @@ const postSchema = new mongoose.Schema<PostType>({
   comment: {
     type: [{
       owner_id: { type: String, required: true},
+      owner_username: { type: String, required: true },
       owner_thumb: { type: String, default: ''},
       contents: { type: String, required: true},
       createdAt: { type: Date, required: true}
@@ -40,7 +44,7 @@ useVirtualId(postSchema);
 
 const Post = mongoose.model('post', postSchema);
 
-export async function findByPostID(postId: string): Promise<PostType | null> {
+export async function findById(postId: string): Promise<PostType | null> {
   return Post.findById(postId);
 }
 
