6172d130e5dbf44e1fefaf377394c394248b70c7
BaeHyukJun <sachun78@naver.com>
2022-04-20 13:31:40 +0900

    * fixed  - 미팅 성사 여부 필드 추가 (status: boolean)

--- BackEnd/src/data/mhistory.ts
+++ BackEnd/src/data/mhistory.ts
@@ -1,20 +1,23 @@
 import mongoose from 'mongoose';
-import * as meetingRepo from 'data/meeting';
+import { useVirtualId } from 'database/database';
 
 interface IMhistory {
   with: [string],
+  status: boolean,
   result: string,
   photopath: string
 };
 
 export const mhistoryScheme = new mongoose.Schema<IMhistory>({
   with: { type: [String], default: ["test1"]},
+  status: { type: Boolean, require: true},
   result: { type: String, require: true},
   photopath: { type: String, default: ''}
 }, {
   timestamps: false,
   versionKey: false
 });
+useVirtualId(mhistoryScheme);
 
 const mhistory = mongoose.model('mhistory', mhistoryScheme);
 
