1ba4cfe8fbf2221bc6514077539fc2a33e42139a
BaeHyukJun <sachun78@naver.com>
2022-04-22 18:16:28 +0900

    * fixed  - history result 페이지에 이미지 업로드 기능 적용

--- BackEnd/src/controller/mhistory.ts
+++ BackEnd/src/controller/mhistory.ts
@@ -1,9 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
 import * as MhisRepo from 'data/mhistory';
 import * as meetingRepo from 'data/meeting';
+import multer from 'multer';
 
 interface IRequest extends Request {
   [key: string]: any
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const upload = multer({
+  storage: storage
+}).single('mhistory_img')
+
+export function profileImage(req: IRequest, res: Response, next: NextFunction) {
+  const mhis_id = req.params.id;
+
+  upload(req, res, (err) => {
+    if (err) {
+      console.error(err)
+      return res.status(409).json({ success: false, error: `${err.code}` })
+    }
+
+    MhisRepo.updateMhistory(mhis_id, { photopath: req.file?.filename });
+    return res.json({
+      success: true,
+      fileName: req.file?.filename
+    })
+  })
 }
 
 export async function createMhistory(req: IRequest, res: Response, next: NextFunction) {
--- BackEnd/src/routes/api/mhistory.ts
+++ BackEnd/src/routes/api/mhistory.ts
@@ -5,5 +5,6 @@ import { isAuth } from 'middleware/authChecker';
 const route = express.Router()
 
 route.post('/:id', isAuth, mMhisCtrl.createMhistory);
+route.post('/upload/:id', isAuth, mMhisCtrl.profileImage);
 
 export default route
