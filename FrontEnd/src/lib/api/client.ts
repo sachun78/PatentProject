83a4c18a8400ac99569d893cdee235d695bb04c7
n27 <skfdkfkalsrn@gmail.com>
2022-04-28 10:01:39 +0900

    로컬

--- FrontEnd/src/lib/api/client.ts
+++ FrontEnd/src/lib/api/client.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const client = axios.create({
-  withCredentials: true
+  withCredentials: true,
 })
 
 // client.defaults.baseURL = 'http://192.168.11.108:4000/'
-client.defaults.baseURL = 'https://wemet-server.herokuapp.com/'/*'http://localhost:4000/'*/
+client.defaults.baseURL = 'http://localhost:8080/' /*'http://localhost:4000/'*/
 
 export default client
