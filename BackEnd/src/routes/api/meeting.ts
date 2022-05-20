1e6cf413f812d7e45aef3b4478d6331a8824af7d
BaeHyukJun <sachun78@naver.com>
2022-05-20 13:54:25 +0900

    * fixed: replan send error  - removed validate email check

--- BackEnd/src/routes/api/meeting.ts
+++ BackEnd/src/routes/api/meeting.ts
@@ -1,15 +1,8 @@
 import express from 'express'
 import * as meetingCtrl from 'controller/meeting'
 import { isAuth } from 'middleware/authChecker';
-import { body } from 'express-validator'
-import { validate } from '../../middleware/validator'
 
 const route = express.Router()
-
-const validateCredential = [
-  body('toEmail').isEmail().normalizeEmail().withMessage('invalid email'),
-  validate
-]
 
 route.get('/', isAuth, meetingCtrl.getMeetings);
 route.get('/history', isAuth, meetingCtrl.getMeetingHistory);
@@ -18,7 +11,7 @@ route.get('/show/:code', meetingCtrl.getMeetingByCode);
 route.get('/cancel/:code', meetingCtrl.cancelMeeting);
 route.get('/confirm/:code', meetingCtrl.confirmMeeting);
 route.patch('/replan/:code', meetingCtrl.replanlMeeting);
-route.post('/send-invitemail', isAuth, validateCredential, meetingCtrl.sendInvitMail);
+route.post('/send-invitemail', isAuth, meetingCtrl.sendInvitMail);
 route.post('/send-resultmail', isAuth, meetingCtrl.sendResultMail);
 
 export default route
